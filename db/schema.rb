d387675300961421da0d0d2678396e2dcd7ea9bf
Jorge Ortiz <ortiz.mata.jorge@gmail.com>
2022-01-14 11:16:32 -0600

    Login up and Sign Up were modified.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_163317) do
+ActiveRecord::Schema.define(version: 2022_01_14_153413) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_01_13_163317) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
